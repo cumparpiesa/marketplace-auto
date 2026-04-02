8e5a541477e33b0dda8be5565b06de17962c2622
Calin Pascala <almaniroadsconstruct@yahoo.com>
2026-04-02 09:47:04 +0300

    fix supabase client path REAL

--- app/lib/supabaseClient.ts
+++ app/lib/supabaseClient.ts
@@ -1,6 +1,6 @@
-import { supabase } from "@/lib/supabaseClientClient"
+import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-)
+);
