e5545407b3eaa53dee683c4bf365bf12e77a6f43
Calin Pascala <almaniroadsconstruct@yahoo.com>
2026-04-02 11:19:06 +0300

    fix navbar layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
-
 export default function RootLayout({ children }: any) {
   return (
     <html>
       <body>
+        <Navbar />   {/* 🔥 AICI ERA PROBLEMA */}
         {children}
       </body>
     </html>
