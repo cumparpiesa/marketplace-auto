9642cfe35bb020667ab2e51e02d2a06e4bab38c7
Calin Pascala <almaniroadsconstruct@yahoo.com>
2026-04-02 09:30:54 +0300

    fix api supabase import

--- app/api/ads/route.ts
+++ app/api/ads/route.ts
@@ -1,6 +1,5 @@
 import { NextResponse } from "next/server"
-import { supabaseServer } from "@/lib/supabaseClientServer"
-
+import { supabaseServer } from "../../../lib/supabaseServer"
 export async function POST(req: Request) {
   const supabase = supabaseServer()
 
